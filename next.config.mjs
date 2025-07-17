f6912c2e3eb4a5d32104e680f564a188b3df4bdc
JungWoo Ham <evaham@naver.com>
2025-07-17 11:03:25 +0900

    next.config.mjs 파일 수정: 이미지 최적화 비활성화 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true';
+
+const nextConfig = {
+    output: 'export', // 반드시 static export 설정
+    basePath: isGithubPages ? '/ms_next' : '',
+    assetPrefix: isGithubPages ? '/ms_next/' : '',
+    images: {
+      unoptimized: true, // next/image 최적화 비활성화 (필수!)
+    }
+};
 
 export default nextConfig;

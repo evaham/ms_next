e9f37b7409810fdd06b4024d00091249fc7e74ee
JungWoo Ham <evaham@naver.com>
2025-07-17 11:54:11 +0900

    next.config.mjs 파일 수정: basePath 및 assetPrefix 설정을 repo 변수를 사용하도록 변경

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const isGithubPages = process.env.NODE_ENV === 'production';
 
+const repo = 'ms_next';
+
 const nextConfig = {
   output: 'export', // static HTML export
-  basePath: isGithubPages ? '/ms_next' : '',
-  assetPrefix: isGithubPages ? '/ms_next/' : '',
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
   images: {
     unoptimized: true, // 이미지 최적화 기능 끄기 (next export 호환)
   },
